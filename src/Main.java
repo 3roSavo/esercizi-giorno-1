50189825f482de4e88c08d86a9ac7dabe21024f0
Eros Savogin <eros.sav@gmail.com>
2023-11-27 17:11:33 +0100

    prova quinto commit

--- src/Main.java
+++ src/Main.java
@@ -11,5 +11,28 @@ public class Main {
 
       int numeroCaratteri = quartaVar.length();
       System.out.println(numeroCaratteri);
+
+
+      // ESERCIZIO 1
+
+        int moltiplica = mul(6, 3);
+        System.out.println("risultato moltiplicazione : " + moltiplica);
+
+        String concatena = conc("Mi chiamo Eros e ho ", 26, " anni");
+        System.out.println(concatena);
+
+        //String[] inserisciLnArray = new String[6];
+
+
+
     }
+    public static int mul(int num1, int num2) {
+        return num1 * num2;
+    }
+    public  static String conc(String frase1, int num, String frase2) {
+        return frase1 + num + frase2;
+    }
+    /*public  static String[] pushIntoArray( String[] {"string1", "string2", "string3", "string4", "string5"}, String string) {
+        return
+    }*/
 }
